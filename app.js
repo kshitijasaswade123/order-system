9703d6cbebb015c641906ff1cb0d69d0b782ef81
kshitijasaswade <61146709+kshitijasaswade123@users.noreply.github.com>
2024-03-28 16:06:00 +0530

    cors import

--- app.js
+++ app.js
@@ -2,9 +2,12 @@ const express = require('express');
 const mongoose = require("mongoose")
 require('dotenv/config');
 const orderRoute = require("./Route/Orde_Route")
+const cors = require("cors");
 
 const app =express();
 app.use(express.json());
+app.use(cors());
+
 app.get('/',(req,res)=>{
     res.send("HOME")
 })
